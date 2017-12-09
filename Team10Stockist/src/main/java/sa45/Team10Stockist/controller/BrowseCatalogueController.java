9337a301cf712e70a659c8b51c733a44fcaff2e0
ystar <ystar@10.211.55.3>
2017-12-09 11:00:32 +0800

    transaction controller

--- Team10Stockist/src/main/java/sa45/Team10Stockist/controller/BrowseCatalogueController.java
+++ Team10Stockist/src/main/java/sa45/Team10Stockist/controller/BrowseCatalogueController.java
@@ -3,6 +3,8 @@ package sa45.Team10Stockist.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -22,6 +25,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import sa45.Team10Stockist.repository.ProductRepository;
 import sa45.Team10Stockist.service.ProductService;
 import sa45.Team10Stockist.model.Product;
+import sa45.Team10Stockist.model.Transaction;
+import sa45.Team10Stockist.model.TransactionDetail;;
 
 
 //TODO
@@ -49,12 +54,37 @@ public class BrowseCatalogueController {
 		//return mav;
 	}
 	
-	@RequestMapping(value = "/catalogue/delete/{partNumber}", method= RequestMethod.GET)
+	/*@RequestMapping(value = "/catalogue/delete/{partNumber}", method= RequestMethod.GET)
 	public ModelAndView deleteProduct(@PathVariable String partNumber, final RedirectAttributes redirectAttributes){
 		
 		pService.removeProduct(pService.findProduct(Integer.parseInt(partNumber)));
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("redirect:/home/catalogue");
+		return mav;
+		
+	}*/
+	
+	@RequestMapping(value = "/catalogue/delete/{partNumber}", method= RequestMethod.GET)
+	public @ResponseBody void deleteProduct(@PathVariable String partNumber, final RedirectAttributes redirectAttributes){
+		
+		pService.removeProduct(pService.findProduct(Integer.parseInt(partNumber)));
+	}
+	
+	@RequestMapping(value = "/catalogue/history/{partNumber}", method= RequestMethod.GET)
+	public ModelAndView historyProduct(@PathVariable String partNumber){
+		ModelAndView mav = new ModelAndView("historyjsp");
+		Product p = pService.findProduct(Integer.parseInt(partNumber));
+		/*ArrayList<TransactionDetail> tlist = (ArrayList<TransactionDetail>)p.getTransactionDetails();*/
+		List<TransactionDetail> tlist = p.getTransactionDetails();
+		
+		HashSet<Transaction> tset = new HashSet<Transaction>();
+		for(TransactionDetail t : tlist) {
+			tset.add(t.getTransaction());
+		}
+		
+		mav.addObject("p", p);
+		mav.addObject("tlist", tlist);
+		mav.addObject("tset", tset);
 		return mav;
 		
 	}
