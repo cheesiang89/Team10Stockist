8069c353a99d9baf66aa6ab9eb291c349d508c5a
Huey Ching Poh <e0231960@u.nus.edu>
2017-12-10 17:27:34 +0800

    Usage Use Case Update

--- Team10Stockist/src/main/java/sa45/Team10Stockist/controller/MechanicController.java
+++ Team10Stockist/src/main/java/sa45/Team10Stockist/controller/MechanicController.java
@@ -1,15 +1,18 @@
 package sa45.Team10Stockist.controller;
 
-import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.inject.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import sa45.Team10Stockist.javabeans.Usage;
 import sa45.Team10Stockist.model.Customer;
 import sa45.Team10Stockist.model.Product;
@@ -30,21 +33,24 @@ public class MechanicController {
 	@RequestMapping(value = "/usage", method = RequestMethod.GET)
 	public ModelAndView newUsagePage() {
 		ModelAndView mav = new ModelAndView("usage");
-//		ArrayList<Customer> customerlist = (ArrayList<Customer>)cservice.createCustomer();
-//		mav.addObject("usage", customerlist);
-		mav.addObject("usage");
+		List<Customer> customerlist = cservice.findAllCustomer();
+		mav.addObject("customerlist",customerlist);
+		List<Product> productlist = pservice.findAllProduct();
+		mav.addObject("productlist", productlist);
 		return mav;
 	}
+
+	@RequestMapping(value = "/usage/{customerId}", method= RequestMethod.GET)
+	public @ResponseBody String findCustomerName(@PathVariable String customerId){
+		return cservice.findCustomer(Integer.parseInt(customerId)).getCustomerName();
+	}
 	
+	/*@RequestMapping(value = "/usage",method = RequestMethod.POST)
+	public ModelAndView postDelivery(BindingResult result, final RedirectAttributes redirectAttributes) {
 	
-	
-//	@RequestMapping(value = "/mechanic/usage",method = RequestMethod.POST)
-//	public ModelAndView createNewTransaction(@ModelAttribute Product product, BindingResult result,
-//		final RedirectAttributes redirectAttributes) {
-//		
 		
-		
-	//}
-	
+		return null;
+	}
+*/
 	
 }
