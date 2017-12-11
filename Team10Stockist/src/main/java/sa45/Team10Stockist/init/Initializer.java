28a0f58a347b09465b577d3ec109d09d5eacd07a
Raymond Tay <Raymond Tay@172.17.250.76>
2017-12-11 10:35:40 +0800

    Initializer fix

--- Team10Stockist/src/main/java/sa45/Team10Stockist/init/Initializer.java
+++ Team10Stockist/src/main/java/sa45/Team10Stockist/init/Initializer.java
@@ -1,6 +1,7 @@
 package sa45.Team10Stockist.init;
 
 import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
@@ -36,15 +37,25 @@ public class Initializer extends AbstractAnnotationConfigDispatcherServletInitia
 		characterEncodingFilter.setEncoding("UTF-8");
 		return new Filter[] { characterEncodingFilter, new SiteMeshFilter() };
 	}
-	 public void onStartup(ServletContext servletContext) throws ServletException {
-	     AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-	     ctx.register(WebAppConfig.class);
-	     ctx.setServletContext(servletContext);
-	     Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
-	     servlet.addMapping("/");
-	     servlet.setLoadOnStartup(1);
-	     servlet.setInitParameter("throwExceptionIfNoHandlerFound", "true");
-	 }
-   
-	
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(WebAppConfig.class);
+		ctx.setServletContext(servletContext);
+		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+		servlet.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+
+		// UTF-8 Encoding
+		FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+		fr.setInitParameter("encoding", "UTF-8");
+		fr.setInitParameter("forceEncoding", "true");
+		fr.addMappingForUrlPatterns(null, true, "/*");
+
+		// SiteMesh
+		fr = servletContext.addFilter("sitemesh", new SiteMeshFilter());
+		fr.addMappingForUrlPatterns(null, true, "/*");
+	}
+
 }
