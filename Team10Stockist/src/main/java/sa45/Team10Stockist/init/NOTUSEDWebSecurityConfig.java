ac5d3960e83f7d1051ce9b3d2a3425560ef7530c
CSLee <CSLee@172.17.250.46>
2017-12-11 11:58:32 +0800

    Internationalization

--- Team10Stockist/src/main/java/sa45/Team10Stockist/init/NOTUSEDWebSecurityConfig.java
+++ Team10Stockist/src/main/java/sa45/Team10Stockist/init/NOTUSEDWebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class NOTUSEDWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
--- Team10Stockist/src/main/webapp/WEB-INF/decorators/menu.jsp
+++ Team10Stockist/src/main/webapp/WEB-INF/decorators/menu.jsp
@@ -1,6 +1,6 @@
 <%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">	
 
 <c:url value="/logout" var="logout" />
